83aaa6c694ae6979b0206b239296dd353f82f54f
Tiany <TianyuZhangAndrew@outlook.com>
2022-11-16 17:31:44 +1100

    Add report

--- test.py
+++ test.py
@@ -1,20 +1,15 @@
 import csv
 
-full_data_file = open("data2.csv", "r", encoding="utf-8")
+full_data_file = open("Combine.csv", "r", encoding="utf-8")
 reader = csv.reader(full_data_file)
 
 global titles
 
 row_number = 0
 data = []
-count = 0
 for row in reader:
     if row_number > 0:
         data.append(row)
-
-        if eval(row[17]) % 2 != 0 and eval(row[26]) % 2 != 0:
-            count += 1
-
     else:
         titles = row
 
@@ -22,18 +17,7 @@ for row in reader:
 
 full_data_file.close()
 
-print(count)
-
-# count = 0
-# for row in data:
-#     if int(row[39]) != 0:
-#         count += 1
-#
-# print(titles[39], count)
-
-
-
-# Robot
+# # Robot
 # name_index_dict = {}
 # for i in range(1, len(titles)):
 #     name_index_dict[titles[i]] = i
@@ -56,64 +40,102 @@ print(count)
 #
 # print(robot_dict)
 
-# correct_count = 0
-# total_count = 0
-# for row in data:
-#     if float(row[14]) == 0:
-#         total_count += 1
-#
-#         if float(row[41]) == 0:
-#             correct_count += 1
-# #
-# #     if float(row[14]) == float(row[41]):
-# #         correct_count += 1
-# #
-# print(titles[14], titles[41])
-# print(correct_count, total_count)
-
-# Camera~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# # Camera~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # room_index = [29, 32, 36, 41]
 # camera_index = [11, 12, 13, 14]
 # cam_dict = {}
 #
+# # Camera motion
+# for j in range(0, len(camera_index)):
+#     cam_dict[titles[camera_index[j]]] = {}
+# 
+#     motion_detect = 0
+#     non_motion_detect = 0
+#     motion_error = 0
+#     non_motion_error = 0
+#     for i in range(1, len(data)):
+#         if float(data[i][camera_index[j]]) != 0:
+#             motion_detect += 1
+# 
+#             if float(data[i][room_index[j]]) == 0:
+#                 motion_error += 1
+# 
+#         else:
+#             non_motion_detect += 1
+# 
+#             if float(data[i][room_index[j]]) != 0:
+#                 non_motion_error += 1
+# 
+#     cam_dict[titles[camera_index[j]]]["True motion"] = (motion_detect - motion_error) / motion_detect
+#     cam_dict[titles[camera_index[j]]]["False motion"] = motion_error / motion_detect
+#     cam_dict[titles[camera_index[j]]]["True non motion"] = (non_motion_detect - non_motion_error) / non_motion_detect
+#     cam_dict[titles[camera_index[j]]]["False non motion"] = non_motion_error / non_motion_detect
+# 
+# for key in cam_dict:
+#     print(key, cam_dict[key])
+#
+# # Camera count, difference and trend
 # for j in range(0, len(camera_index)):
 #     cam_dict[titles[camera_index[j]]] = {}
 #
 #     correct_diff_count = 0
-#     correct_change_count = 0
-#     for i in range(1, len(data)):
-#         camera_diff = float(data[i][camera_index[j]]) - float(data[i - 1][camera_index[j]])
-#         actual_diff = float(data[i][room_index[j]]) - float(data[i - 1][room_index[j]])
+#     correct_trend_count = 0
+#     correct_count = 0
 #
-#         if (camera_diff * actual_diff > 0) or (camera_diff == 0 and actual_diff == 0):
-#             correct_change_count += 1
+#     total_diff = 0
+#     total_trend = 0
+#     total_count = 0
+#     for i in range(0, len(data)):
 #
-#         if camera_diff == actual_diff:
-#             correct_diff_count += 1
+#         total_count += 1
+#         if float(data[i][camera_index[j]]) == float(data[i][room_index[j]]):
+#             correct_count += 1
 #
-#     cam_dict[titles[camera_index[j]]]["Correct difference count"] = correct_diff_count
-#     cam_dict[titles[camera_index[j]]]["Correct change count"] = correct_change_count
+#         if i >= 1:
+#             camera_diff = float(data[i][camera_index[j]]) - float(data[i - 1][camera_index[j]])
+#             actual_diff = float(data[i][room_index[j]]) - float(data[i - 1][room_index[j]])
 #
+#             total_trend += 1
+#             total_diff += 1
+#
+#             if (camera_diff * actual_diff > 0) or (camera_diff == 0 and actual_diff == 0):
+#                 correct_trend_count += 1
+#
+#             if camera_diff == actual_diff:
+#                 correct_diff_count += 1
+#
+#     cam_dict[titles[camera_index[j]]]["Correct difference"] = correct_diff_count / total_diff
+#     cam_dict[titles[camera_index[j]]]["Correct trend"] = correct_trend_count / total_trend
+#     cam_dict[titles[camera_index[j]]]["Correct count"] = correct_count / total_count
 #
 # for key in cam_dict:
 #     print(key, cam_dict[key])
 
-# Motion sensor~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# # Motion sensor~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # motion_sensor_dict = {}
 # for i in range(1, 11):
 #     motion_sensor_dict[titles[i]] = {}
 #
-#     error_count = 0
-#     motion_count = 0
+#     motion_detect = 0
+#     non_motion_detect = 0
+#     motion_error = 0
+#     non_motion_error = 0
 #     for row in data:
-#         if int(row[28 + i]) == 0:
-#             motion_count += 1
+#         if row[i] == "motion":
+#             motion_detect += 1
 #
-#             if row[i] == "motion":
-#                 error_count += 1
+#             if int(row[28 + i]) == 0:
+#                 motion_error += 1
+#         else:
+#             non_motion_detect += 1
 #
-#     motion_sensor_dict[titles[i]]["Total motion count"] = motion_count
-#     motion_sensor_dict[titles[i]]["Error count"] = error_count
+#             if int(row[28 + i]) != 0:
+#                 non_motion_error += 1
+#
+#     motion_sensor_dict[titles[i]]["True motion"] = (motion_detect - motion_error) / motion_detect
+#     motion_sensor_dict[titles[i]]["False motion"] = motion_error / motion_detect
+#     motion_sensor_dict[titles[i]]["True non motion"] = (non_motion_detect - non_motion_error) / non_motion_detect
+#     motion_sensor_dict[titles[i]]["False non motion"] = non_motion_error / non_motion_detect
 #
 # for key in motion_sensor_dict:
 #     print(key, motion_sensor_dict[key])
